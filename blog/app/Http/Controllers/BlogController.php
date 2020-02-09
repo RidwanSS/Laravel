c35dc7027cef84731f4b682aa27b760cb1f0b9ed
Ridwan Riri <ridwan@macbook-air-de-ridwan.home>
2020-02-09 12:45:21 +0100

    blog laravel

---
+++ blog/app/Http/Controllers/BlogController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->only('addArticle');
+    }
+
+    /**
+     * Show the application blog.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('blog');
+    }
+
+    /**
+     * Show the article add page.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function addArticle()
+    {
+        return view('blog.add');
+    }
+}
